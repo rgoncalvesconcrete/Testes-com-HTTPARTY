8ae1ce185bc732d0fac79def7b89ed405e69a8c7
Rodolfo Gonçalves <rodolfo.goncalves@concrete.com.br>
2018-02-01 17:13:56 -0200

    atualização

--- testeDeAPI/teste_api.rb
+++ testeDeAPI/teste_api.rb
@@ -1,12 +1,39 @@
 require 'httparty'
-#require 'json'
 
-response = HTTParty.get('https://pokeapi.co/api/v2/pokemon/1/')
+class TesteAPI
+  include HTTParty
+  base_uri 'https://pokeapi.co/api/v2'
+end
+
+RSpec.describe 'Teste de API - GET' do
+  it 'Retornando um Pokemon' do
+    begin
+      response = TesteAPI.get('/pokemon/1/')
+      expect(response.code).to eql(200)
+      expect(response['name']).to eql('bulbasaur')
+
+      puts response
+    end
+  end
+end
+
+# RSpec.describe 'Teste de API - GET' do
+#     it 'Retornando um Pokemon 222' do
+#       begin
+#         response = TesteAPI.get('/pokemon/-1/')
+#         expect(response.code).to eql(200)
+#         #xpect(response['name']).to eql(200)
+#       end
+#     end
+#   end
+
+# require 'json'
+
+# response = HTTParty.get('https://pokeapi.co/api/v2/pokemon/1/')
 # puts response.body, response.code, response.message, response.headers.inspect
 
-
-puts "Status HTTP: #{response.code}"
-puts "Status Mensagem: #{response.message}"
+# puts "Status HTTP: #{response.code}"
+# puts "Status Mensagem: #{response.message}"
 
 # dados = response.parsed_response['forms']
 
@@ -14,6 +41,6 @@ puts "Status Mensagem: #{response.message}"
 
 # dados.each do |valor|
 #   puts valor['name']
-#  # puts valor["weight"] 
+#  # puts valor["weight"]
 
 # end
